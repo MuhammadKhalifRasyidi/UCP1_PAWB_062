ea676c4ae1ba8709e915ff9c4186ac44d9d7ac49
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-23 23:08:36 +0700

    update terbaru pada controller-home

--- src/routes/router-home.js
+++ src/routes/router-home.js
@@ -6,6 +6,7 @@ const controllerCatatanBibit = require('../controllers/controller-catatanBibit')
 
 
 router.get('/', verifyUser.isLogin, homeController.home);
+router.get('/home', verifyUser.isLogin, homeController.getHome);
 router.get('/catatanPupuk', verifyUser.isLogin, controllerCatatanPupuk.getCatatanPupuk);
 router.get('/catatanBibit', verifyUser.isLogin, controllerCatatanBibit.getCatatanBibit);
 
