5a7707ffe614c67f0d0a1af39338c6ca4c138b00
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-23 23:07:54 +0700

    update terbaru pada controller catatan pupuk

--- src/controllers/controller-catatanPupuk.js
+++ src/controllers/controller-catatanPupuk.js
@@ -18,7 +18,7 @@ module.exports ={
                 if (results.length > 0) {
                     res.render('CatatanPupuk', {
                         url: 'http://localhost:3000/',
-                        catatanPupuk: results // Pass the contacts data to the view
+                        catatanPupuk: results // Pass the CatatanPupuk data to the view
                     });
                 } else {
                     res.render('CatatanPupuk', {
@@ -42,7 +42,7 @@ module.exports ={
             pool.getConnection(function (err, connection) {
                 if (err) throw err;
                 connection.query(
-                    `INSERT INTO contacts (namaPetani, lokasiLahan, luasLahan, jenisTanaman) VALUES (?, ?, ?, ?);`,
+                    `INSERT INTO CatatanPupuk (namaPetani, lokasiLahan, luasLahan, jenisTanaman) VALUES (?, ?, ?, ?);`,
                     [namaPetani, lokasiLahan, luasLahan, jenisTanaman], 
                     function (error, results) {
                         if (error) {
@@ -66,7 +66,7 @@ module.exports ={
         const { id } = req.params;
         pool.getConnection(function (err, connection) {
             if (err) throw err;
-            connection.query('SELECT * FROM contacts WHERE id = ?', [id], function (error, results) {
+            connection.query('SELECT * FROM CatatanPupuk WHERE id = ?', [id], function (error, results) {
                 if (error) throw error;
                 if (results.length > 0) {
                     res.render('editCatatanPupuk', {
@@ -86,7 +86,7 @@ module.exports ={
         pool.getConnection(function (err, connection) {
             if (err) throw err;
             connection.query(
-                'UPDATE contacts SET namaPetani = ?, lokasiLahan = ?, luasLahan = ?, jenisTanaman = ? WHERE id = ?',
+                'UPDATE CatatanPupuk SET namaPetani = ?, lokasiLahan = ?, luasLahan = ?, jenisTanaman = ? WHERE id = ?',
                 [namaPetani, lokasiLahan, luasLahan, jenisTanaman, id],
                 function (error, results) {
                     if (error) throw error;
@@ -100,7 +100,7 @@ module.exports ={
         const { id } = req.params;
         pool.getConnection(function (err, connection) {
             if (err) throw err;
-            connection.query('DELETE FROM contacts WHERE id = ?', [id], function (error, results) {
+            connection.query('DELETE FROM CatatanPupuk WHERE id = ?', [id], function (error, results) {
                 if (error) throw error;
                 res.redirect('/CatatanPupuk');
             });
