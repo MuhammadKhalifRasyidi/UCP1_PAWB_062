3ada7fbbdb21bc4670058587ac6e56f0e266335b
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-11-23 23:07:40 +0700

    update terbaru pada controller catatan bibit

--- src/controllers/controller-catatanBibit.js
+++ src/controllers/controller-catatanBibit.js
@@ -18,7 +18,7 @@ module.exports ={
                 if (results.length > 0) {
                     res.render('CatatanBibit', {
                         url: 'http://localhost:3000/',
-                        catatanBibit: results // Pass the contacts data to the view
+                        catatanBibit: results // Pass the Catatan Bibit data to the view
                     });
                 } else {
                     res.render('CatatanBibit', {
@@ -42,7 +42,7 @@ module.exports ={
             pool.getConnection(function (err, connection) {
                 if (err) throw err;
                 connection.query(
-                    `INSERT INTO contacts (namaPetani, lokasiLahan, luasLahan, jenisTanaman) VALUES (?, ?, ?, ?);`,
+                    `INSERT INTO Catatan Bibit (namaPetani, lokasiLahan, luasLahan, jenisTanaman) VALUES (?, ?, ?, ?);`,
                     [namaPetani, lokasiLahan, luasLahan, jenisTanaman], 
                     function (error, results) {
                         if (error) {
@@ -66,7 +66,7 @@ module.exports ={
         const { id } = req.params;
         pool.getConnection(function (err, connection) {
             if (err) throw err;
-            connection.query('SELECT * FROM contacts WHERE id = ?', [id], function (error, results) {
+            connection.query('SELECT * FROM Catatan Bibit WHERE id = ?', [id], function (error, results) {
                 if (error) throw error;
                 if (results.length > 0) {
                     res.render('editCatatanBibit', {
@@ -86,7 +86,7 @@ module.exports ={
         pool.getConnection(function (err, connection) {
             if (err) throw err;
             connection.query(
-                'UPDATE contacts SET namaPetani = ?, lokasiLahan = ?, luasLahan = ?, jenisTanaman = ? WHERE id = ?',
+                'UPDATE Catatan Bibit SET namaPetani = ?, lokasiLahan = ?, luasLahan = ?, jenisTanaman = ? WHERE id = ?',
                 [namaPetani, lokasiLahan, luasLahan, jenisTanaman, id],
                 function (error, results) {
                     if (error) throw error;
@@ -100,7 +100,7 @@ module.exports ={
         const { id } = req.params;
         pool.getConnection(function (err, connection) {
             if (err) throw err;
-            connection.query('DELETE FROM contacts WHERE id = ?', [id], function (error, results) {
+            connection.query('DELETE FROM Catatan Bibit WHERE id = ?', [id], function (error, results) {
                 if (error) throw error;
                 res.redirect('/CatatanBibit');
             });
